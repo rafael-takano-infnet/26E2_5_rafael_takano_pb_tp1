a4ce8627a396dc9f3040c32d6a85095de5dcaaa7
rafael-takano-infnet <rafael.takano@al.infnet.edu.br>
2026-09-23 12:37:13 -0300

    Adiciona CORS no review-service e log de consumo do evento book.deleted

--- review-service/src/main/java/com/reviewservice/messaging/BookEventListener.java
+++ review-service/src/main/java/com/reviewservice/messaging/BookEventListener.java
@@ -2,11 +2,15 @@ package com.reviewservice.messaging;
 
 import com.reviewservice.config.RabbitMQConfig;
 import com.reviewservice.service.ReviewService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BookEventListener {
+
+    private static final Logger log = LoggerFactory.getLogger(BookEventListener.class);
 
     private final ReviewService reviewService;
 
@@ -16,6 +20,7 @@ public class BookEventListener {
 
     @RabbitListener(queues = RabbitMQConfig.BOOK_DELETED_QUEUE)
     public void onBookDeleted(Long bookId) {
+        log.info("Evento book.deleted recebido: removendo avaliacoes do livro {}", bookId);
         reviewService.deleteByBookId(bookId);
     }
 }
